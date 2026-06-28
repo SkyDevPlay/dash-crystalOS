3c0e47c866f7a6ce892ce01a96d9edd69df31de3
SkyDev167 <skydevpro@outlook.fr>
2026-06-28 21:33:27 +0200

    Update ports.asm

--- krnl/sys/ports.asm
+++ krnl/sys/ports.asm
@@ -4,6 +4,8 @@ global inb
 global outb
 global inw
 global outw
+global inl
+global outl
 
 inb:
     mov edx, [esp + 4]
@@ -21,8 +23,20 @@ inw:
     xor eax, eax
     in ax, dx
     ret
+
 outw:
     mov edx, [esp + 4]
     mov eax, [esp + 8]
     out dx, ax
+    ret
+
+inl:
+    mov edx, [esp + 4]
+    in eax, dx
+    ret
+
+outl:
+    mov edx, [esp + 4]
+    mov eax, [esp + 8]
+    out dx, eax
     ret
