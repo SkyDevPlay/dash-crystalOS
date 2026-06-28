3ba37b43def15ef9e4443330cdeb584f4610b5a9
SkyDev167 <skydevpro@outlook.fr>
2026-06-28 19:59:36 +0200

    Update kernel.c

--- krnl/kernel.c
+++ krnl/kernel.c
@@ -55,6 +55,9 @@ int main(void) {
         printf("Serial port failed to initialize\n");
     }
 
+    printf("lba_start = %d\n", mbr->parts[0].lba_start);
+    putchar('A'); putchar('\n');
+
     if (init_fs(mbr->parts[0].lba_start) < 0) {
         setColor(RED);
         printf("WARNING FATsystem unmounted(lba=%d)\n", mbr->parts[0].lba_start);
