d867ea309ea8c1e2360d929cc50edb16da9d30b8
SkyDev167 <skydevpro@outlook.fr>
2026-06-28 21:34:15 +0200

    Update ports.h

--- include/sys/ports.h
+++ include/sys/ports.h
@@ -4,14 +4,15 @@
 #include "types.h"
 
 #define PIC1_COMMAND 0x20
-#define PIC1_DATA 0x21
+#define PIC1_DATA    0x21
 #define PIC2_COMMAND 0xA0
-#define PIC2_DATA 0xA1
+#define PIC2_DATA    0xA1
 
-u8 inb(u16 port);
+u8   inb(u16 port);
 void outb(u16 port, u8 value);
-u16 inw(u16 port);
+u16  inw(u16 port);
 void outw(u16 port, u16 value);
-
+u32  inl(u16 port);
+void outl(u16 port, u32 value);
 
 #endif
