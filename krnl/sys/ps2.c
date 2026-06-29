1802e0494b07e3cca4f467841dd506dedac98601
SkyDev167 <skydevpro@outlook.fr>
2026-06-29 23:08:16 +0200

    ps2 fix

--- krnl/sys/ps2.c
+++ krnl/sys/ps2.c
@@ -5,6 +5,50 @@
 
 struct IDT_entry IDT[256];
 
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+
+void (*int_table[20])(void) = {
+    isr0,
+    isr1,
+    isr2,
+    isr3,
+    isr4,
+    isr5,
+    isr6,
+    isr7,
+    isr8,
+    isr9,
+    isr10,
+    isr11,
+    isr12,
+    isr13,
+    isr14,
+    isr15,
+    isr16,
+    isr17,
+    isr18,
+    isr19
+};
+
 void int_handle() {
     puts("\n\nOH NO AN EXCEPTION\n\n");
     while (1);
@@ -17,12 +61,12 @@ void init_idt() {
     IDT[0x21].offset_lower = (u32)keyboard_handler & 0xFFFF;
     IDT[0x21].offset_upper = ((u32)keyboard_handler & 0xFFFF0000) >> 16;
 
-    for(int i = 0; i < 0x20; i++) {
+    for(int i = 0; i < 20; i++) {
         IDT[i].segment = 8;
         IDT[i].zero = 0;
         IDT[i].type = 0b10001110;
-        IDT[i].offset_lower = (u32)int_handler & 0xFFFF;
-        IDT[i].offset_upper = ((u32)int_handler & 0xFFFF0000) >> 16;
+        IDT[i].offset_lower = (u32)int_table[i] & 0xFFFF;
+        IDT[i].offset_upper = ((u32)int_table[i] & 0xFFFF0000) >> 16;
     }
 
     // Restart PICs
