54d2d88535758b48b305968a41864768b2e93158
SkyDev167 <skydevpro@outlook.fr>
2026-07-01 09:36:48 +0200

    paging fix

--- include/sys/paging.h
+++ include/sys/paging.h
@@ -7,4 +7,7 @@
 #define PAGE_RW (1 << 1)
 #define PAGE_USER (1 << 2)
 
+extern u32 page_directory[1024]_attribute_((aligned(4096)));
+extern u32 page_table[1024]_attribute_((aligned(4096)));
+
 #endif
--- krnl/sys/paging.c
+++ krnl/sys/paging.c
@@ -1,1 +1,4 @@
 #include "paging.h"
+
+u32 page_directory[1024]_attribute_((aligned(4096)));
+u32 page_table[1024]_attribute_((aligned(4096)));
